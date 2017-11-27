2cf941036cdad7f2227c9cb51f5c07160bd5a467
Mats Jensen <mats.jensen@evry.com>
2017-11-27 13:32:06 +0100

    13 -Understanding state done

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    constructor() {
+        super();
+        this.createFish = this.createFish.bind(this);
+        /*
+            If you dont want a constructor you can put this instead : onSubmit={(e) => this.createFish(e)}
+        */
+    }
+    createFish(event) {
+        event.preventDefault();
+        console.log('Gonna make some fish 🐠');
+        // take values from input field and create fish object on every submit
+        const fish = {
+            name: this.name.value,
+            price: this.price.value,
+            status: this.status.value,
+            desc: this.desc.value,
+            image: this.image.value
+        }
+        this.props.addFish(fish);
+        this.fishForm.reset();
+    }
+    render() {
+        return(
+            <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={this.createFish}>
+                <input ref={(input) => {this.name = input}} type="text" placeholder="Fish Name" />
+                <input ref={(input) => {this.price = input}} type="text" placeholder="Fish Price" />
+                <select ref={(input) => {this.status = input}}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea ref={(input) => {this.desc = input}} type="text" placeholder="Fish Desc"></textarea>
+                <input ref={(input) => {this.image = input}} type="text" placeholder="Fish Image" />
+                <button type="submit">+ Add Item</button>
+            </form>
+        )
+    }
+}
+
+export default AddFishForm;
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,29 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+
+        //get initial state
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+    addFish(fish) {
+        // update state
+            // spread current state of fishes into variable
+        const fishes = {...this.state.fishes};
+            // add new fish
+        const timestamp = Date.now();
+        //give it unique name
+        fishes[`fish-${timestamp}`] = fish;
+
+        //set state
+        this.setState({ fishes })
+    }
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +35,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }

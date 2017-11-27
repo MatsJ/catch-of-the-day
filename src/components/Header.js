0826d84448fc23001faecd477894f52e9235cbe2
Mats Jensen <matsjensen@Matss-MacBook-Pro.local>
2017-11-27 12:09:59 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/*
+    If you dont need other methods other than render
+    you dont need an entire react component
+    instead use a stateless functional component like this:
+    function Header {(code)} OR like below
+*/
+    const Header  = (props) => {
+        return(
+            <header className="top">
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                    Day
+                </h1>
+                <h3 className="tagline"><span>{props.tagline}</span></h3>
+            </header>
+        )
+    }
+        
+
+export default Header;

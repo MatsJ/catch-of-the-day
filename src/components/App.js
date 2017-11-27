4ecc80f75b07fac8f2ae612274c9e3810d847dd0
Mats Jensen <mats.jensen@evry.com>
2017-11-27 15:00:26 +0100

    14, 15 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 
+import sampleFishes from '../sample-fishes';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
 
         //get initial state
         this.state = {
             fishes: {},
             order: {}
         };
+    }
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        })
     }
     addFish(fish) {
         // update state
@@ -33,10 +41,19 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="list-of-fishes">
+                    {/* loop over fishes */}
+                        {/* gets current fish and passes in details about that fish */}
+                        {
+                            Object
+                            .keys(this.state.fishes)
+                            .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                        }
+                    </ul>
                 </div>
                 <Order />
                 {/* Passing function down to Inventory.js */}
-                <Inventory addFish={this.addFish}/>
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
     }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -8,6 +8,7 @@ class Inventory extends React.Component {
                 <h2>Inventory</h2>
                 {/* Pass function down to AddFishForm */}
                 <AddFishForm addFish={this.props.addFish}/>
+                <button onClick={this.props.loadSamples}>Load Sample fishes</button>
             </div>
             
         )

e7ca9ef612919787b9148d33c5799992ec00ece8
Mats Jensen <mats.jensen@evry.com>
2018-03-02 14:12:01 +0100

    1,2,3,4,5,6,7,8,9,10 done 😎

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+    render() {
+        return (
+            <div className="inventory">
+            
+            </div>
+        );
+    }
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="order">
+            
+            </div>
+        )
+    }
+}
+
+export default Order;
